67a470f586cb21de5f8232588f75d8eb3a1bd283
Ryan Johnston <ryanj@ryanj.org>
2019-12-09 14:48:15 -0700

    Updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Johnston Private Cloud</h1>
       </header>
+      <section>
+        <h2>Entertainment</h2>
+        <ul>
+          <li>
+            <a
+              href="http://plex.ryanj.net:32400/web/index.html#"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Plex
+            </a>
+          </li>
+          <li>
+            <a href="http://couchpotato:5050/" target="_blank" rel="noreferrer noopener">
+              Couch Potato
+            </a>
+          </li>
+        </ul>
+        <h2>Documents and Files</h2>
+        <ul>
+          <li>
+            <a href="http://nextcloud/" target="_blank" rel="noopener noreferrer">
+              Document Storage &amp; Sharing
+            </a>
+          </li>
+          <li>
+            <a href="http://freenas/" target="_blank" rel="noopener noreferrer">
+              Network Storage System
+            </a>
+          </li>
+        </ul>
+        <h2>Downloaders</h2>
+        <ul>
+          <li>
+            <a href="http://sabnzb:8080/" target="_blank" rel="noopener noreferrer">
+              SabNzb
+            </a>
+          </li>
+        </ul>
+        <h2>Search</h2>
+        <ul>
+          <li>
+            <a href="http://magneticod:8080" target="_blank" rel="noopener noreferrer">
+              Magnetico Torrent Search
+            </a>
+          </li>
+          <li>
+            <a href="http://searx" target="_blank" rel="noopener noreferrer">
+              Web Search
+            </a>
+          </li>
+          <li>
+            <a href="http://hydra:5076/" target="_blank" rel="noopener noreferrer">
+              NZB Hydra
+            </a>
+          </li>
+        </ul>
+        <h2>System</h2>
+        <ul>
+          <li>
+            <a href="http://pihole/admin/index.php" target="_blank" rel="noopener noreferrer">
+              Pi-hole Ad Blocking
+            </a>
+          </li>
+          <li>
+            <a href="http://znc:9999" target="_blank" rel="noopener noreferrer">
+              ZNC
+            </a>
+          </li>
+        </ul>
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
